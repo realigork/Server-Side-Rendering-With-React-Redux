2ef3ac790307f2686e30eb7013dd9d7a8f4a56f3
Igor Kolga <igorkolga@hotmail.com>
2019-12-16 12:45:36 +0000

    Add comments to webpack server config

--- webpack.server.js
+++ webpack.server.js
@@ -1,12 +1,19 @@
 const path = require('path');
 
 module.exports = {
+  // Inform Webpack that we're building a bundle for Node
   target: 'node',
+
+  // Root file of server app
   entry: './src/index.js',
+
+  // Where to put the output file
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
   },
+
+  // 
   module: {
     rules: [
       {
