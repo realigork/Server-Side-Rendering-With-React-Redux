efc408e5996783a15fbbb6b697062992c6215fdb
Igor Kolga <igorkolga@hotmail.com>
2019-12-16 13:26:57 +0000

    Instruct Express server to download static files from public folder and update rendering of initial HTML to include the bundle from public folder

--- src/index.js
+++ src/index.js
@@ -6,9 +6,26 @@ import Home from './client/components/Home';
 
 const app = express();
 
+// Tell Express to use 'public' folder as the one available
+// to show to the outside world from which it will download
+// bundle.js
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   const content = renderToString(<Home />);
-  res.send(content);
+
+  // Tell Express to download the public bundle
+  const html = `
+    <html>
+      <head></head> 
+      <body>
+        <div>${content}</div>
+        <script src="bundle.js"></script>
+      </body>
+    </html>
+  `;
+
+  res.send(html);
 });
 
 app.listen(3000, () => {
