5e0a1434822ae23283e2a9dfaa7c7eaa0dc0b8d0
Igor Kolga <igorkolga@hotmail.com>
2019-12-16 14:13:06 +0000

    Render the Client App so event handlers for the button start to work

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>I am Home</div>
+  return (
+    <div>
+      <div>I am NEW 4 Home</div>
+      <button onClick={() => console.log('Hi there!')}>Press me</button>
+    </div>
+  );
 };
 
 export default Home;
