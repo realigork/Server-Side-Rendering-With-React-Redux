47b2adba651e246f4a384df52900d01fec03e32b
Igor Kolga <igorkolga@hotmail.com>
2019-12-17 15:57:50 +0000

    Prevent adding node_modules to server bundle using webpack-node-externals

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
   // Inform Webpack that we're building a bundle for Node
@@ -13,7 +14,11 @@ const config = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-  }
+  },
+
+  // Tell Webpack to not bundle any libs into the output bundle if that
+  // lib exists in node_modules
+  externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
