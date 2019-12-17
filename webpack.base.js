129bc62803e04471e2a996c2d889b4da88c10baf
Igor Kolga <igorkolga@hotmail.com>
2019-12-17 15:47:57 +0000

    Refactor webpack configs by adding base config and merging it with client and server configs

---
+++ webpack.base.js
@@ -0,0 +1,26 @@
+module.exports = {
+  // Run Babel on every file
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+            [
+              'env',
+              {
+                targets: {
+                  browsers: ['last 2 versions']
+                }
+              }
+            ]
+          ]
+        }
+      }
+    ]
+  }
+}
--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
 
-module.exports = {
+const config = {
   // Inform Webpack that we're building a bundle for Node
   target: 'node',
 
@@ -11,30 +13,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-  },
-
-  // 
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            'react',
-            'stage-0',
-            [
-              'env',
-              {
-                targets: {
-                  browsers: ['last 2 versions']
-                }
-              }
-            ]
-          ]
-        }
-      }
-    ]
   }
 };
+
+module.exports = merge(baseConfig, config);
