1f7a8ed86d20233c65c5efd0801b21f693a0768e
Igor Kolga <igorkolga@hotmail.com>
2019-12-27 14:05:31 +0000

    Add axios instance to the client and pass it to thunk middleware to reveal axios as a third argument in action creators when making requests to API; Update fetchUsers action creator

--- src/client/client.js
+++ src/client/client.js
@@ -3,6 +3,7 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
+import axios from 'axios';
 import { BrowserRouter } from 'react-router-dom'
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
@@ -11,10 +12,17 @@ import { renderRoutes } from 'react-router-config';
 import Routes from './Routes';
 import reducers from './reducers';
 
+// Request to /api is proxied in Express
+const axiosInstance = axios.create({
+  baseURL: '/api'
+})
+
+// Pass this axiosInstance to thunk middleware as a third argument
+// so it becomes available in action creators
 const store = createStore(
   reducers,
   window.INITIAL_STATE,
-  applyMiddleware(thunk)
+  applyMiddleware(thunk.withExtraArgument(axiosInstance))
 );
 
 ReactDOM.hydrate(
