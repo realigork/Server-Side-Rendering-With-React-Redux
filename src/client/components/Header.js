cb29fca871d9a0b0b8f22cac05c8a7d5b3b37082
Igor Kolga <igorkolga@hotmail.com>
2019-12-29 19:36:00 +0000

    Add materialize.css and apply styles to header

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -8,14 +8,24 @@ const Header = ({ auth }) => {
     : <a href="/api/auth/google">Login</a>;
 
   return (
-    <div>
-      <Link to="/">React SSR</Link>
-      <div>
-        <Link to="/users">Users</Link>
-        <Link to="/admins">Admins</Link>
-        {authButton}
+    <nav>
+      <div className="nav-wrapper">
+        <Link to="/" className="brand-logo">React SSR</Link>
+        <ul class="right">
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+
+          <li>
+            <Link to="/admins">Admins</Link>
+          </li>
+
+          <li>
+            {authButton}
+          </li>
+        </ul>
       </div>
-    </div>
+    </nav>
   );
 };
 
--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -18,7 +18,9 @@ export default (req, store) => {
   // Tell Express to download the public bundle
   return `
     <html>
-      <head></head> 
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+      </head> 
       <body>
         <div id="root">${content}</div>
         <script>
