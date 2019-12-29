dab7a10f6b5504016941a4ab4c1a7215bf302e6e
Igor Kolga <igorkolga@hotmail.com>
2019-12-29 19:57:40 +0000

    Render context object on the app and look if it has the propety NotFound set as true to send 404 network status

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -6,10 +6,10 @@ import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config'; // this will consume plain JS routes and turn them to JSX components
 import Routes from '../client/Routes';
 
-export default (req, store) => {
+export default (req, store, context) => {
   const content = renderToString(
     <Provider store={store}>
-      <StaticRouter location={req.path} context={{}}>
+      <StaticRouter location={req.path} context={context}>
         <div>{renderRoutes(Routes)}</div>
       </StaticRouter>
     </Provider>
