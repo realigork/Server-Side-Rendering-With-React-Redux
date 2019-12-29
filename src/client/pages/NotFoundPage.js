065bcfefc4b2c8f6080f7a6703002851631b3f66
Igor Kolga <igorkolga@hotmail.com>
2019-12-29 19:51:29 +0000

    Render NotFound page when route is not found

---
+++ src/client/pages/NotFoundPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <div className="center-align">
+      <h3>Oops!</h3>
+      <p>Page not found</p>
+    </div>
+  );
+};
+
+export default {
+  component: NotFoundPage
+};
